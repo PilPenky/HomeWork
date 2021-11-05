4e59d65bb4c4f4b3dbdab8a315ac19bfe4c97010
PilPenky <apg-a@mail.ru>
2021-11-05 16:34:33 +0200

    done!

--- ConsoleApp22/Program.cs
+++ ConsoleApp22/Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApp22
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Для вычисления среднесуточной температуры воздуха выполните слудующие действия:");
+            Console.WriteLine("1. Введите минимальное значение температуры за сутки в °C");
+            string input = Console.ReadLine();
+            int x = int.Parse(input);
+            Console.WriteLine("2. Введите максимальное значение температуры за сутки в °C");
+            string input1 = Console.ReadLine();
+            int y = int.Parse(input1);
+            Console.WriteLine($"Среднесуточная температуры воздуха составляет {(y + x) / 2}°C");
         }
     }
 }
