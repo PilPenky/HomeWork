a6340437803a0458ec739de35d6a4b36a568083d
PilPenky <apg-a@mail.ru>
2021-11-05 17:38:01 +0200

    done 2.2

--- ConsoleApp22/Program.cs
+++ ConsoleApp22/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 
-namespace ConsoleApp22
+namespace ConsoleApp12
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Укажите порядковый номер месяца для выведения его названия на экран.");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Январь");
+                    break;
+                case "2":
+                    Console.WriteLine("Февраль");
+                    break;
+                case "3":
+                    Console.WriteLine("Март");
+                    break;
+                case "4":
+                    Console.WriteLine("Апрель");
+                    break;
+                case "5":
+                    Console.WriteLine("Май");
+                    break;
+                case "6":
+                    Console.WriteLine("Июнь");
+                    break;
+                case "7":
+                    Console.WriteLine("Июль");
+                    break;
+                case "8":
+                    Console.WriteLine("Август");
+                    break;
+                case "9":
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case "10":
+                    Console.WriteLine("Октябрь");
+                    break;
+                case "11":
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case "12":
+                    Console.WriteLine("Декабрь");
+                    break;
+
+            }
+
         }
     }
 }
