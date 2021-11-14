cac3eedc5a9f70e6c860246cc75fdf30579266fc
PilPenky <apg-a@mail.ru>
2021-11-14 22:18:29 +0200

    done4.2

--- ConsoleApp22/Program.cs
+++ ConsoleApp22/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace ConsoleApp22
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Вас приветствует программа суммирования чисел.");
+            Console.WriteLine("Введите числа через пробел, которые хотите сложить:");
+            string str = Console.ReadLine();
+            int result = 0;
+            int number = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                var click = str[i];
+                if (click != ' ')
+                {
+                    int x = click - '0';
+                    number = number * 10 + x;
+                }
+                else
+                {
+                    result += number;
+                    number = 0;
+                }
+            }
+            if (number > 0)
+            {
+                result += number;
+            }
+            Console.WriteLine(result);
         }
     }
 }
