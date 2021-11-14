c5960662c80b6b82f89e32d5a7df3e664dad67f5
PilPenky <apg-a@mail.ru>
2021-11-14 22:16:55 +0200

    done4.1

--- ConsoleApp22/Program.cs
+++ ConsoleApp22/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace ConsoleApp22
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string GetFullName(string firstName, string lastName, string patronymic)
+            {
+                if (firstName != null)
+                {
+                    return firstName;
+                }
+                else
+                if (lastName != null)
+                {
+                    return lastName;
+                }
+                else
+                {
+                    return patronymic;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Введите фамилию: ");
+                string firstName1 = Console.ReadLine();
+                Console.Write("Введите имя: ");
+                string lastName1 = Console.ReadLine();
+                Console.Write("Введите отчество: ");
+                string patronymic1 = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine($"{GetFullName(firstName1, null, null)} {GetFullName(null, lastName1, null)} {GetFullName(null, null, patronymic1)}");
+                Console.WriteLine();
+
+            }
+
+
         }
     }
 }
