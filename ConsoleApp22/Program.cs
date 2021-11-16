d7de156b11808cb1e8d7a184fcd1692dafc5fe79
PilPenky <apg-a@mail.ru>
2021-11-16 16:05:42 +0200

    done5.1

--- ConsoleApp22/Program.cs
+++ ConsoleApp22/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿/*                                                                   Задание 1 к уроку 5
+ *                                          Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+ */
+using System;
+using System.IO;
+using System.Text.Json;
 
-namespace ConsoleApp22
+namespace ConsoleApp11
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string json = JsonSerializer.Serialize(Console.ReadLine());
+            File.WriteAllText("dataone.txt", json);
         }
     }
 }
